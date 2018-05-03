4a63edb8893e78a6a343a9fddcaa2a219d3c5b00
Benoît Legat <benoit.legat@gmail.com>
2018-05-03 11:02:34 +0200

    Add doc

---
+++ docs/make.jl
@@ -0,0 +1,22 @@
+using Documenter, EntropicCone
+
+makedocs(
+    format = :html,
+    sitename = "EntropicCone",
+    pages = [
+        "Index" => "index.md",
+        "Introduction" => "intro.md",
+    ],
+    # The following ensures that we only include the docstrings from
+    # this module for functions define in Base that we overwrite.
+    modules = [EntropicCone]
+)
+
+deploydocs(
+    repo   = "github.com/blegat/EntropicCone.jl.git",
+    target = "build",
+    osname = "linux",
+    julia  = "0.6",
+    deps   = nothing,
+    make   = nothing
+)
---
+++ docs/src/index.md
@@ -0,0 +1,9 @@
+# SumOfSquares --- Entropic Cone
+
+[EntropicCone](https://github.com/blegat/EntropicCone.jl) utilities for constructing and using outer and inner approximations of the Entropic Cone.
+
+## Contents
+```@contents
+Pages = ["intro.md"]
+Depth = 2
+```
